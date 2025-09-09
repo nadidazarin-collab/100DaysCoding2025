6fb13d9f09c00952c2e0ba02fae0b606a05732ac
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-09 18:04:26 +0800

    day2.java

---
+++ day2.java
@@ -0,0 +1,19 @@
+package day2ofcoding;
+public class Main {
+    public static void main(String[] args) {
+        
+   String nama;
+   int umur;
+   float tb;
+   
+   nama = "Nadida Zarin Mahira";
+   umur = 18;
+   tb = 150.0f ;
+   
+        System.out.println("assalamualaikum");
+        System.out.print("perkenalkan");
+        System.out.printf(" nama saya: %s, umur: %d, tinggi badan: %.1f", nama, umur, tb);
+   
+      }
+    
+}
