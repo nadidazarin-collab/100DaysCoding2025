10cfa1b24688ee30a584ad8e8b99ac21aee105e4
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-10 10:51:16 +0800

    day3.java

---
+++ day3.java
@@ -0,0 +1,19 @@
+public class Javaday3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Komentar untuk satu baris
+        /*Komentar lebih dari satu baris*/
+        
+        // Menampilkan pesan untuk mencetak nama
+        System.out.println("Nadida Zarin Mahira");
+        
+        String nama;
+        nama = "Nadida Zarin Mahira";
+        // Menampilkan Pesan sapaan
+        System.out.println("Halo nama saya : " + nama);
+    }
+    
+}
