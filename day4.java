2527b4f324981a1e59410ef4f4005043024eb9cb
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-11 16:22:23 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,21 @@
+package javaday4;
+public class Javaday4 {
+    public static void main(String[] args) {
+        // \n digunakan untuk membuat baris baru
+        System.out.println("Ini baris pertama.\nIni baris kedua.");
+        
+        // \t digunakan untuk membuat tab atau spasi  
+        System.out.println("Nadida\t18thn\tMajene");
+        
+        /* \\ (Backslash) Digunakan untuk
+           menampilkan 1 karakter backlash itu sendiri.
+        */
+        System.out.println("Lokasi file: C:\\Users\\user\\file.txt"); 
+        
+        // \" (Double Quote)Digunakan untuk menampilkan tanda kutip ganda.
+        System.out.println("Hai kak, \"Selamat sore!\"");
+        System.out.println("maaf kak, \"Sore ini saya ada kerkom "
+                + " bukan tidak mau belajar\"");
+    }
+    
+}
