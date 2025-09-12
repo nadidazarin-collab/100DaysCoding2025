757f914e52c9d864e972ed2d81165303d11b1740
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-12 20:14:23 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,16 @@
+package javaday5;
+public class Javaday5 {
+    public static void main(String[] args) {
+        byte umur = 18;
+        int TanggalLahir = 310507;
+        short angkaKeberuntungan = 20;
+        long jarakAkudanDia = 150000000L;
+        
+        System.out.println("umur:" + umur);
+        System.out.println("tanggal lahir:" + TanggalLahir);
+        System.out.println("angka keberuntungan:" + angkaKeberuntungan);
+        System.out.println("jarak aku dan dia:" + jarakAkudanDia);
+        // TODO code application logic here
+    }
+    
+}
