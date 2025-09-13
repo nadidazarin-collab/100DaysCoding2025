4328117d9dabd10cea219c8d19cd8cb5a6e62148
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-13 23:25:39 +0800

    day6.java

---
+++ day6.java
@@ -0,0 +1,19 @@
+package day6_java;
+
+ public class TipeDataPecahan {
+    public static void main(String[] args) {
+
+        
+        float a = 12.345f;
+       
+        // Menampilkan nilai float
+        System.out.println(" suhu dini hari: " + a);
+        
+        double b = 12345.6789;
+       
+        // Menampilkan nilai double
+        System.out.println(" luas bumi: " + b);
+        
+    }
+}
+   
