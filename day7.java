385849f22085296efc748e9386721e28fe212e2f
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-14 15:43:50 +0800

    day7.java

---
+++ day7.java
@@ -0,0 +1,29 @@
+package javaday7;
+public class Javaday7 {
+    private static boolean d;
+        public static void main(String[] args) {
+        // Tipe data char 
+        char huruf = 'N';
+        char simbol = '@';
+
+        // Menampilkan nilai tipe data char
+        System.out.println("Inisial nama saya: " + huruf);
+           System.out.println("nama ig saya: " + simbol + "nadidazarin");
+        
+        // Tipe data boolean (true/false)
+        boolean a = true; 
+        boolean b = false; 
+
+        // Menampilkan nilai tipe data boolean
+        System.out.println(" \nsaya suka tidur: " + a);
+        System.out.println("saya suka tugas: " + b);
+
+        // Operasi sederhana 
+        boolean c = (5 > 3);
+        d = (3 > 5);
+        System.out.println(" \nApakah 5 lebih besar dari 3? " + c);
+        System.out.println("Apakah 3 lebih besar dari 5? " + d);
+    
+    }
+    
+}
