27ce3131655102ff96536334b0ad17af1ddb717e
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-15 21:24:38 +0800

    day8.java

---
+++ day8.java
@@ -0,0 +1,19 @@
+package javaday8;
+public class Javaday8 {
+    public static void main(String[] args) {
+         //String
+        String nama = "Nadida Zarin Mahira";
+        String status = "Mahasiswa";
+        String hobi = "Menyanyi.";
+
+        // Menampilkan nilai String
+        System.out.println("Nama: " + nama);
+        System.out.println("Status: " + status);
+        System.out.println("Hobi: " + hobi);
+
+        // Mengubah jadi sebuah teks
+        String gabungan = nama + " adalah seorang " + status + " yang suka " + hobi ;
+        System.out.println("Gabungan teks: " + gabungan);
+    }
+    
+}
