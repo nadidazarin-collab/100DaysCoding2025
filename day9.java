0dcf551ff76a4d274e881df289e289d337f39245
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-16 20:54:40 +0800

    day9.java

---
+++ day9.java
@@ -0,0 +1,22 @@
+public class day9java {
+    public static void main(String[] args) {
+        // Deklarasi variabel (tanpa nilai awal)
+        int angka;
+
+        // Inisialisasi variabel (memberikan nilai awal)
+        angka = 02;
+        System.out.println("Nilai awal angka: " + angka);
+
+        // Update variabel (mengubah nilai variabel)
+        angka = 07;
+        System.out.println("Nilai angka setelah di-update: " + angka);
+
+        // Deklarasi dan inisialisasi 
+        String nama = "Nadida";
+        System.out.println("Nama: " + nama);
+
+        // Update nilai variabel String
+        nama = "Zarin";
+        System.out.println("Nama setelah di-update: " + nama);
+    }
+}
