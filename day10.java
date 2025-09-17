a02d6dfb0540e4a757675c234e5d6f3ab87a696e
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-17 21:09:24 +0800

    day10.java

---
+++ day10.java
@@ -0,0 +1,15 @@
+public class day10java {
+    public static void main(String[] args) {
+    
+    /*konstanta = variabel yang nilainya tidak dapat
+      diubah setelah dideklarasikan*/
+    public static final double Phi = 3.14;
+    public static final String nama = "Nadida Zarin Mahira";
+    public static final int tahunLahir= 2007;
+  
+        // Menggunakan konstanta
+        System.out.println("Nilai Phi: " + Phi);
+        System.out.println("Nama Pengguna: " + nama);
+        System.out.println("Tahun Lahir: " + tahunLahir);
+    }
+      }
