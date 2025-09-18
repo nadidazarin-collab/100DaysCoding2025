512dfab90d53199433ff9fde569fd25aa0ad612a
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-18 19:08:36 +0800

    day11.java

--- day11.java
+++ day11.java
@@ -4,19 +4,22 @@ public class Javaday11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Nadida Zarin Mahira");
+        System.out.print("Masukkan nama: ");
         String nama = scanner.nextLine();
 
-        System.out.print("18");
+        System.out.print("Masukkan umur: ");
         int umur = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
-        System.out.print("Tunda, Belakang stadion");
+        System.out.print("Masukkan alamat: ");
         String alamat = scanner.nextLine();
-
+       
+        /*output dari codingn ini akan meminta kita memasukkan
+        nama, umur & alamat sesuai data kita*/
         System.out.println("Halo saya, " + nama + "!");
         System.out.println("Umur saya: " + umur + " tahun");
         System.out.println("Alamat saya: " + alamat);
-      
+
+
     }
 }
