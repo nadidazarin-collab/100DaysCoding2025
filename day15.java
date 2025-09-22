019bffedc759914bcdb48efea46335c1402ffb0a
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-09-22 20:02:47 +0800

    day15.java

--- day15.java
+++ day15.java
@@ -1,18 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Mengubah string menjadi tipe data primitif
-        String strAngka1 = "10";
-        String strAngka2 = "5";
-
-        // string ke integer
-        int angka1 = Integer.parseInt(strAngka1); 
-        int angka2 = Integer.parseInt(strAngka2); 
+        int angka1= 10;
+        int angka2 = 5;
 
         // Operator aritmatika penjumlahan dan pengurangan
         int jumlah = angka1 + angka2;
         int kurang = angka1 - angka2; 
 
-        
         System.out.println("Hasil penjumlahan: " + jumlah); 
         System.out.println("Hasil pengurangan: " + kurang); 
 
