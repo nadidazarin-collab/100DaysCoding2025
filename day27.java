cf54364e82769a1b097d0f2c2c4ebb5652a811d2
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-10-04 20:21:04 +0800

    day27.java

---
+++ day27.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day27 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("");
+        int P = in.nextInt();
+        System.out.print("");
+        int L = in.nextInt();
+        
+        System.out.println("Jumlah keramik minimal yang dibutuhkan " + (P*L)/4);
+        
+    }
+    
+}
