5003ef99da59e66bf518686882ba115fc2dd1a8b
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-10-26 21:10:23 +0800

    operator ternary

---
+++ day49.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ternary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+        System.out.println("Angka " + angka + " adalah " + hasil + ".");
+    }
+}
