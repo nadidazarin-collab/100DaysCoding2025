be1f25d65905d37c71b7f8a32169671311e64f5a
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-10-28 22:21:27 +0800

    perulangan for

---
+++ day51.java
@@ -0,0 +1,9 @@
+public class PerulanganFor {
+    public static void main(String[] args) {
+        System.out.println("Angka dari 1 hingga 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka: " + i);
+        }
+    }
+}
