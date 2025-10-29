ce16647b26e7f47fb81bf3c985ee7d9fce0f4069
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-10-29 21:49:37 +0800

    perulangan while

---
+++ day52.java
@@ -0,0 +1,12 @@
+public class PerulanganWhile {
+    public static void main(String[] args) {
+        int i = 1;
+
+        System.out.println("Angka dari 1 hingga 10:");
+
+        while (i <= 10) {
+            System.out.println("Angka: " + i);
+            i++;
+        }
+    }
+}
