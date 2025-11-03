df59bd6165f398d47ab79b0660097d14e21c16de
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-11-03 23:30:35 +0800

    nested loop

---
+++ day57.java
@@ -0,0 +1,12 @@
+public class PolaBintang {
+    public static void main(String[] args) {
+        int baris = 5;
+
+        for (int i = 1; i <= baris; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
