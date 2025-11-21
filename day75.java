ccb1d7b9194b64118b5d38de4e9a29790a6bf337
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-11-21 18:20:55 +0800

    length uppercase lowercase

---
+++ day75.java
@@ -0,0 +1,14 @@
+public class StringDasarday75 {
+    public static void main(String[] args) {
+        String nama = "Nadida Zarin Mahira";
+
+        int panjang = nama.length();
+        System.out.println("Panjang string: " + panjang);
+
+        String upperCase = nama.toUpperCase();
+        System.out.println("Huruf besar: " + upperCase);
+
+        String lowerCase = nama.toLowerCase();
+        System.out.println("Huruf kecil: " + lowerCase);
+    }
+}
