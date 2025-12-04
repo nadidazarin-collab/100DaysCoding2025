5b5b9810a27edf01e9640f5442dccdf33105e16e
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-12-04 21:19:56 +0800

    akses array for each

---
+++ day88.java
@@ -0,0 +1,10 @@
+public class day88 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Elemen array:");
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
