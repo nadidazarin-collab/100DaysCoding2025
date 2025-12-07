890df1383976557ffb40ba1ee76a4a81245f4c71
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-12-08 00:07:42 +0700

    return

---
+++ day92.java
@@ -0,0 +1,9 @@
+public class day92 {
+    public static int getAngka() {
+        return 10;     
+    }
+    public static void main(String[] args) {
+        int hasil = getAngka();  
+        System.out.println("Angka yang dikembalikan: " + hasil);
+    }
+}
